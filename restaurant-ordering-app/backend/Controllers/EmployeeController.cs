b40b28bb197cd2747e2b5008553a49a7ef5f1154
leyden <fusionsoccer05@gmail.com>
2025-01-14 12:08:38 -0600

    adding models

---
+++ restaurant-ordering-app/backend/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+namespace RestaurantOrderingApp.Backend.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using RestaurantOrderingApp.Backend.Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly List<Employee> Employees = new()
+        {
+            new Employee { Id = 1, FirstName = "John", LastName = "Johnbouy" },
+            new Employee { Id = 2, FirstName = "Steve", LastName = "Stevie" }
+        };
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            return Ok(Employees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee([FromBody] Employee Employee)
+        {
+            Employee.Id = Employees.Count + 1;
+            Employees.Add(Employee);
+            return CreatedAtAction(nameof(GetEmployees), new { id = Employee.Id }, Employee);
+        }
+    }
+}
--- restaurant-ordering-app/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ restaurant-ordering-app/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97c85dc6ac19fea894f28e371f6ced65bf17705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+686978bfde10989a2b4d12f6eefd5ba538ca9ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
